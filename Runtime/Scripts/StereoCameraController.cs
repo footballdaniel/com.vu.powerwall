fee5a1e301b17d0c29a5760c6e7f7c1accf6e982
Daniel Müller <daniel.daniel@gmx.ch>
2024-09-09 18:23:23 +0200

    refactor input, camera controller

--- Runtime/Scripts/StereoCameraController.cs
+++ Runtime/Scripts/StereoCameraController.cs
@@ -2,49 +2,52 @@
 
 public class StereoCameraController : MonoBehaviour
 {
-    [Header("External dependencies"), SerializeField] XRTracker _motionTracker;
-    [SerializeField] ProjectionPlane _projectionPlane;
-    
-    [Header("Internal dependencies"),SerializeField] Camera _cameraLeft;
-    [SerializeField] Camera _cameraRight;
-    [SerializeField] ProjectionMatrix _projectionPlaneLeft;
-    [SerializeField] ProjectionMatrix _projectionPlaneRight;
-    
-    void Awake() => Application.targetFrameRate = 120;
+	[Header("External dependencies"), SerializeField] XRTracker _motionTracker;
+	[SerializeField] ProjectionPlane _projectionPlane;
 
-    private void Start()
-    {
-        transform.SetParent(_motionTracker.transform);
-        transform.localPosition = Vector3.zero;
-        
-        _projectionPlaneLeft.Set(_projectionPlane.gameObject);
-        _projectionPlaneRight.Set(_projectionPlane.gameObject);
-    }
+	[Header("Internal dependencies"), SerializeField] Camera _cameraLeft;
+	[SerializeField] Camera _cameraRight;
+	[SerializeField] ProjectionMatrix _projectionPlaneLeft;
+	[SerializeField] ProjectionMatrix _projectionPlaneRight;
 
-    void FixedUpdate()
-    {
-        if (_active3D)
-            SwitchBetweenCameras();
-    }
-    
-    void SwitchBetweenCameras()
-    {
-        if (_displayLeftEyeImage)
-        {
-            _cameraLeft.Render();
-            _cameraLeft.enabled = true;
-            _cameraRight.enabled = false;
-            _displayLeftEyeImage = false;
-        }
-        else
-        {
-            _cameraRight.Render();
-            _cameraRight.enabled = true;
-            _cameraLeft.enabled = false;
-            _displayLeftEyeImage = true;
-        }
-    }
+	bool _active3D;
+	bool _displayLeftEyeImage;
 
-    bool _active3D;
-    bool _displayLeftEyeImage;
+	void Awake()
+	{
+		Application.targetFrameRate = 120;
+	}
+
+	void Start()
+	{
+		transform.SetParent(_motionTracker.transform);
+		transform.localPosition = Vector3.zero;
+
+		_projectionPlaneLeft.Set(_projectionPlane.gameObject);
+		_projectionPlaneRight.Set(_projectionPlane.gameObject);
+	}
+
+	void FixedUpdate()
+	{
+		if (_active3D)
+			SwitchBetweenCameras();
+	}
+
+	void SwitchBetweenCameras()
+	{
+		if (_displayLeftEyeImage)
+		{
+			_cameraLeft.Render();
+			_cameraLeft.enabled = true;
+			_cameraRight.enabled = false;
+			_displayLeftEyeImage = false;
+		}
+		else
+		{
+			_cameraRight.Render();
+			_cameraRight.enabled = true;
+			_cameraLeft.enabled = false;
+			_displayLeftEyeImage = true;
+		}
+	}
 }
--- Runtime/Scripts/XRTracker.cs
+++ Runtime/Scripts/XRTracker.cs
@@ -2,44 +2,39 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-// using Valve.VR;
-
 [Serializable]
-public class XRTracker : MonoBehaviour 
+public class XRTracker : MonoBehaviour
 {
-    
-    
-    [SerializeField] InputActionReference _positionAction;
-    [SerializeField] bool _isXForward;
+	[SerializeField] InputActionReference _positionAction;
+	[SerializeField] bool _isXForward;
 
-    void OnEnable()
-    {
-        _positionAction.action.Enable();
-    }
-    
-    void OnDisable()
-    {
-        _positionAction.action.Disable();
-    }
+	void Update()
+	{
+		var position = _positionAction.action.ReadValue<Vector3>();
 
-    private void Update()
-    {
-        var position = _positionAction.action.ReadValue<Vector3>();
-        
-        if (_isXForward)
-            transform.localPosition = new Vector3(position.z, position.y, -position.x);
-        else
-            transform.localPosition = position;
-        
-    }
-    
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, transform.forward);
-        
-        // gizmo in z direction
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(transform.position, transform.up);
-    }
+		if (_isXForward)
+			transform.localPosition = new Vector3(position.z, position.y, -position.x);
+		else
+			transform.localPosition = position;
+	}
+
+	void OnEnable()
+	{
+		_positionAction.action.Enable();
+	}
+
+	void OnDisable()
+	{
+		_positionAction.action.Disable();
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay(transform.position, transform.forward);
+
+		// gizmo in z direction
+		Gizmos.color = Color.blue;
+		Gizmos.DrawRay(transform.position, transform.up);
+	}
 }
