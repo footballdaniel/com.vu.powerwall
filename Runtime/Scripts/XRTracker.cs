79b3de53117ee87ce78c10d401ab0141917aea61
Daniel Müller <daniel.daniel@gmx.ch>
2024-12-09 10:34:46 +0100

    expose  position on tracker

--- Runtime/Scripts/XRTracker.cs
+++ Runtime/Scripts/XRTracker.cs
@@ -7,19 +7,10 @@ public class XRTracker : MonoBehaviour
 {
 	[SerializeField] InputActionReference _positionAction;
 	[SerializeField] bool _isXForward;
-	
+
 	[field: SerializeReference] public string TrackerMappingName { get; private set; }
-	
 
-	void Update()
-	{
-		var position = _positionAction.action.ReadValue<Vector3>();
-
-		if (_isXForward)
-			transform.localPosition = new Vector3(position.z, position.y, -position.x);
-		else
-			transform.localPosition = position;
-	}
+	public Vector3 Position { get; set; }
 
 	void OnEnable()
 	{
@@ -29,6 +20,17 @@ public class XRTracker : MonoBehaviour
 	void OnDisable()
 	{
 		_positionAction.action.Disable();
+	}
+
+
+	void Update()
+	{
+		Position = _positionAction.action.ReadValue<Vector3>();
+
+		if (_isXForward)
+			transform.localPosition = new Vector3(Position.z, Position.y, -Position.x);
+		else
+			transform.localPosition = Position;
 	}
 
 	void OnDrawGizmos()
