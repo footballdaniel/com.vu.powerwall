2b03a7be6bb6d84f65cf8807851b6d368b99a3b8
Daniel Müller <daniel.daniel@gmx.ch>
2025-01-15 20:22:16 +0100

    syntax improvements on powerwall

--- Runtime/Scripts/Persistence.cs
+++ Runtime/Scripts/Persistence.cs
@@ -1,85 +1,87 @@
 ﻿using System;
+using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 internal class Persistence
 {
-    public void Save(CalibrationData calibrationData)
-    {
-        var json = JsonConvert.SerializeObject(calibrationData, new Vector3QuaternionConverter());
-        System.IO.File.WriteAllText("calibration.json", json);
-    }
 
-    public CalibrationData TryLoadCalibration()
-    {
-        if (System.IO.File.Exists("calibration.json"))
-        {
-            var json = System.IO.File.ReadAllText("calibration.json");
-            var deserialized = JsonConvert.DeserializeObject<CalibrationData>(json, new Vector3QuaternionConverter());
-            if (deserialized != null)
-                return deserialized;
-        }
+	class Vector3QuaternionConverter : JsonConverter
+	{
 
-        return new CalibrationData(Vector3.zero, Quaternion.identity);
-    }
+		public override bool CanConvert(Type objectType)
+		{
+			return objectType == typeof(Vector3) || objectType == typeof(Quaternion);
+		}
 
-    private class Vector3QuaternionConverter : JsonConverter
-    {
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            var jsonObject = JObject.Load(reader);
-            if (objectType == typeof(Vector3))
-            {
-                return new Vector3(
-                    (float)jsonObject["x"],
-                    (float)jsonObject["y"],
-                    (float)jsonObject["z"]
-                );
-            }
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			var jsonObject = JObject.Load(reader);
 
-            if (objectType == typeof(Quaternion))
-            {
-                return new Quaternion(
-                    (float)jsonObject["x"],
-                    (float)jsonObject["y"],
-                    (float)jsonObject["z"],
-                    (float)jsonObject["w"]
-                );
-            }
+			if (objectType == typeof(Vector3))
+			{
+				return new Vector3(
+					(float)jsonObject["x"],
+					(float)jsonObject["y"],
+					(float)jsonObject["z"]
+				);
+			}
 
-            throw new NotSupportedException($"Cannot convert object of type {objectType}");
-        }
+			if (objectType == typeof(Quaternion))
+			{
+				return new Quaternion(
+					(float)jsonObject["x"],
+					(float)jsonObject["y"],
+					(float)jsonObject["z"],
+					(float)jsonObject["w"]
+				);
+			}
 
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            var jsonObject = new JObject();
+			throw new NotSupportedException($"Cannot convert object of type {objectType}");
+		}
 
-            if (value is Vector3 v3)
-            {
-                jsonObject["x"] = v3.x;
-                jsonObject["y"] = v3.y;
-                jsonObject["z"] = v3.z;
-            }
-            else if (value is Quaternion q)
-            {
-                jsonObject["x"] = q.x;
-                jsonObject["y"] = q.y;
-                jsonObject["z"] = q.z;
-                jsonObject["w"] = q.w;
-            }
-            else
-            {
-                throw new NotSupportedException($"Cannot convert object of type {value.GetType()}");
-            }
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			var jsonObject = new JObject();
 
-            jsonObject.WriteTo(writer);
-        }
+			if (value is Vector3 v3)
+			{
+				jsonObject["x"] = v3.x;
+				jsonObject["y"] = v3.y;
+				jsonObject["z"] = v3.z;
+			}
+			else if (value is Quaternion q)
+			{
+				jsonObject["x"] = q.x;
+				jsonObject["y"] = q.y;
+				jsonObject["z"] = q.z;
+				jsonObject["w"] = q.w;
+			}
+			else
+				throw new NotSupportedException($"Cannot convert object of type {value.GetType()}");
 
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == typeof(Vector3) || objectType == typeof(Quaternion);
-        }
-    }
-    }
+			jsonObject.WriteTo(writer);
+		}
+	}
 
+	public void Save(CalibrationData calibrationData)
+	{
+		var json = JsonConvert.SerializeObject(calibrationData, new Vector3QuaternionConverter());
+		File.WriteAllText("calibration.json", json);
+	}
+
+	public CalibrationData TryLoadCalibration()
+	{
+		if (File.Exists("calibration.json"))
+		{
+			var json = File.ReadAllText("calibration.json");
+			var deserialized = JsonConvert.DeserializeObject<CalibrationData>(json, new Vector3QuaternionConverter());
+
+			if (deserialized != null)
+				return deserialized;
+		}
+
+		return new CalibrationData(Vector3.zero, Quaternion.identity);
+	}
+}
